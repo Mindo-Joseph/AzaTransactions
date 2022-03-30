c43c890a4abae654da580ddfca8e45947f896b25
Mindo-Joseph <mindo.joseph.mj@gmail.com>
2022-03-30 17:54:55 +0300

    Add the show service to display the specific transaction

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -3,4 +3,8 @@ class TransactionsController < ApplicationController
     @transactions = Transaction.all
     render json: @transactions
   end
+  def show
+    @transaction = Transaction.find(params[:id])
+    render json: @transaction
+  end
 end
